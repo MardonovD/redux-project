bfd105f82a9e927d4c78904a0b7e3fb54fc73c55
Dostonbek <mardonovdoston69@gmail.com>
2022-05-09 07:48:49 +0300

    create form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 function App() {
   return (
-    <div className="App">
-      <h1> Hello World ..</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <div className="card">
+            <div className="card-header">
+              <h1>Todos:0</h1>
+            </div>
+            <div className="card-body"></div>
+            <div className="card-footer"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Description.js
+++ src/Description.js
@@ -1,3 +1,4 @@
 /// DESCRIPTIONS..
 //1.react proyectda redux dan foydalanish bunda redux va action degan papkalarni ochib olamiza.
 //2.actionsda actionlar joylashgan boladi reducersda esa reducerla joylashadi..
+//3.bizga ikkita kutubxona kk boladi birinchisi npm i redux va react-redux
