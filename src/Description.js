a7b10588895a266195999578fc432191e44206cc
Dostonbek <mardonovdoston69@gmail.com>
2022-05-09 07:37:17 +0300

    structura

---
+++ src/Description.js
@@ -0,0 +1,3 @@
+/// DESCRIPTIONS..
+//1.react proyectda redux dan foydalanish bunda redux va action degan papkalarni ochib olamiza.
+//2.actionsda actionlar joylashgan boladi reducersda esa reducerla joylashadi..
