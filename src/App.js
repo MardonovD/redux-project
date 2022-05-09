6773adf471af5931722e02b44a36a1274521ad60
Dostonbek <mardonovdoston69@gmail.com>
2022-05-09 08:08:52 +0300

    form

--- src/App.js
+++ src/App.js
@@ -7,18 +7,34 @@ function App() {
             <div className="card-header">
               <h1>Todos:0</h1>
             </div>
-            <div className="card-body"></div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">
+            <div className="card-body">
+              <form className="mb-3 d-flex justify-content-between align-items-center">
+                <div>
+                 
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="yoz.. "
+                  />
+                </div>
+                <button className="btn btn-success">Add</button>
+              </form>
+            </div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
                 <input
-                  class="form-check-input"
+                  className="form-check-input me-2"
                   type="checkbox"
                   value=""
                   id="flexCheckDefault"
                 />
+                <label className="form-check-label" for="flexCheckDefault">
+                  Default checkbox
+                </label>
               </li>
-              <li class="list-group-item">A second item</li>
-              <li class="list-group-item">A third item</li>
+              <li className="list-group-item">A second item</li>
+              <li className="list-group-item">A third item</li>
             </ul>
             <div className="card-footer">
               <button className="btn btn-primary">Save</button>
